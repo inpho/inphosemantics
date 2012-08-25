7f4172239c78b8af097f8c34cc13882d462e764f
Robert Rose <rrose1@indiana.edu>
2012-08-25 14:17:28 -0400

    Stub for the CLI

---
+++ inphosemantics/__init__.py
@@ -0,0 +1,40 @@
+"""
+TODO: Help the user!
+
+"""
+
+#TODO: Install and import vsm (and tell the user to do it as well)
+
+
+print 'Welcome to InPhO Semantics!'
+
+import inphosemantics
+
+
+def available_corpora():
+    """
+    """
+    corpora = request_corpora()
+
+    print corpora
+
+
+
+
+def cheatsheet():
+    """
+    """
+    return
+
+
+
+######################################################################
+#                              Backend
+######################################################################
+
+
+def request_corpora():
+
+    corpora = ['sep', 'iep', 'philpapers']
+
+    return corpora
