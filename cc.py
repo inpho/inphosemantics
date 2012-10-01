623f2d0995499caef6e7e054d324ccf9bdff86d4
Sam Zorowitz <szorowi1@gmail.com>
2012-10-01 11:22:03 -0400

    Updated corpus client program

--- cc.py
+++ cc.py
@@ -4,18 +4,23 @@
 def corpus_client(search_term, disp_metadata=False):
     '''
     '''
-    #x = plain_corpus_names()
-    x = ['dogs', 'cats', 'birds', 'dogs-herding', 'cats-hypoallergenic', 'dogs-no_fur', 'dogs-American', 'cats-American', 'birds-tropical', 'birds-talking', 'cats-bengal']
     import re, os
-
-    x.sort()
-    corpora_index = {}
-    num = range(len(x))
-    for i in num:
-	    corpora_index[num[i]+1]=x[i]
+    
+    plain_corpora = ['dogs', 'cats', 'birds']
+    #plain_corpora = plain_corpus_names()
+    plain_corpora.sort()
+    plain_corpora_index = {}
+    num1 = range(len(plain_corpora))
+    for i in num1:
+	    plain_corpora_index[num1[i]+1]=plain_corpora[i]
    
-    corpus_name = ''
-    while corpus_name == '':	    
+
+    vsm_corpora = ['dogs', 'cats', 'birds', 'dogs-herding', 'cats-hypoallergenic', 'dogs-no_fur', 'dogs-American', 'cats-American', 'birds-tropical', 'birds-talking', 'cats-bengal']
+    #vsm_corpora = vsm_corpus_names()
+    vsm_corpora.sort()
+    
+    plain_corpus_name = ''
+    while plain_corpus_name == '':	    
 	    try:
 	    	    search_term = int(search_term)
 	    except ValueError:
@@ -26,16 +31,17 @@ def corpus_client(search_term, disp_metadata=False):
 		    print 'Inphosemantics Corpus-Client'
 		    print '============================'
 		    print '\nSEARCH RESULTS'
-		    print '==============='
-		    print 'Search Term:', search_term
+		    print '=============='
+		    print 'Index Number:', search_term
 		    try:
-			    corpus_name = corpora_index[search_term]
+			    plain_corpus_name = plain_corpora_index[search_term]
 		    except KeyError:
 			    print 'Call number out of range!'
 		    else:
-			    print search_term, corpus_name
+			    print search_term, plain_corpus_name
 			    if disp_metadata:
-				    print 'Metadata', corpus_metadata(corpus_name)
+				    print 'Metadata:', 'Testing...'
+				    #print 'Metadata', corpus_metadata(plain_corpus_name)
 			    else:
 				    pass
 			    break
@@ -45,24 +51,25 @@ def corpus_client(search_term, disp_metadata=False):
 		    print 'Inphosemantics Corpus-Client'
 		    print '============================'
 		    print '\nSEARCH RESULTS'
-		    print '==============='
+		    print '=============='
 		    print 'Search Term:', search_term
-		    for key, value in corpora_index.iteritems():
+		    for key, value in plain_corpora_index.iteritems():
 			    if re.search(search_term, value) != None:
 				    print key, value
 				    if disp_metadata:
-					    print 'Metadata', corpus_metadata(value)
+					    print 'Metadata:', 'Testing...'
+					    #print 'Metadata:', corpus_metadata(value)
 				    else:
 					    pass
 			    else:
 				    hit = hit + 1
-		    if hit == len(corpora_index.keys()):
+		    if hit == len(plain_corpora_index.keys()):
 			    print 'Term not found!' 
 	    else:
 		    print 'Invalid input: Argument must be integer or string!'
 	    
 	    print '\n---------------'
-	    print 'Enter call number or search search_term.'
+	    print 'Enter call number or search term.'
 	    print '[To end client, press "ctrl+d"]'
 	    try:
 		    new_search = raw_input()
@@ -73,5 +80,18 @@ def corpus_client(search_term, disp_metadata=False):
 		    search_term = new_search[0]
 		    try:
 		    	    disp_metadata = new_search[1]
-		    except KeyError:
-		    	    pass
+		    except IndexError:
+		    	    disp_metadata = False
+
+
+    if plain_corpus_name == '':
+	    pass
+    else:
+	    num2 = 1
+	    print '\nAVAILABLE MATRICES'
+	    print '=================='
+	    for i in vsm_corpora:
+		    if re.search(plain_corpus_name, i) != None:
+			    print num2, i
+			    num2 = num2 + 1
+
